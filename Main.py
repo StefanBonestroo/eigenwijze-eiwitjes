f568ea1ac5fd6ccfb739629cdd59b1f6e2c0c51c
bonymacaroni <31651652+bonymacaroni@users.noreply.github.com>
2017-10-31 13:58:19 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,2 +1,35 @@
-# eigenwijze-eiwitjes
-Heuristieken 2017
+class Protein:
+
+    def __init__(self, proteinChain):
+
+        self.proteinChain = proteinChain
+
+
+    def makeChainStructure (self):
+
+        for amino in self.proteinChain:
+            
+
+
+    def calculateFolding (self):
+
+
+    def visualizeFolding (self):
+
+
+class Amino(Protein):
+
+    def __init__(self, upper, right, lower, left):
+        upper = None
+        right = None
+        lower = None
+        left = None
+
+        molecule =
+
+
+
+
+main()
+protie = Protein("PPHHHPHHHPH")
+protie.culculateFolding
