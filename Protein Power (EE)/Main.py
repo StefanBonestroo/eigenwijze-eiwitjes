0751773c26672d9a12125725c8f590741880d094
bonymacaroni <stefan.bonestroo@student.uva.nl>
2017-11-14 14:37:10 +0100

    De structuur werkt op de score na

--- Protein Power (EE)/Main.py
+++ Protein Power (EE)/Main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python
+
+from classes import Protein
+from functions import visualizeFolding
+from algorithms import randomizer
+
+eggwhite = Protein("HHHPPHHH")
+
+output = randomizer(eggwhite, 200)
+eggwhite.aminoCoordinates = output[0]
+eggwhite.strength = output[1]
+
+visualizeFolding(eggwhite)
--- Protein Power (EE)/functions.py
+++ Protein Power (EE)/functions.py
@@ -1,60 +1,83 @@
-from random import randint
 import matplotlib
 import matplotlib.pyplot as plot
-import Classes.Protein as Protein
+import matplotlib.patches as mpatches
 
 # This function takes a string ('proteinChain') and a set of coordinates ('output')
 # and uses these to present the user with a visualization of the protein and its H-bonds
-def visualizeFolding (self, proteinChain, output):
+def visualizeFolding (Protein):
+
+    proteinChain = Protein.proteinChain
+    x = 0
+    y = 1
+
+    plotX = []
+    plotY = []
 
     # Iterates over all single amino acids in the protein
     for focus in range(len(proteinChain)):
 
-        current_x = output['x-cor'][focus]
-        current_y = output['y-cor'][focus]
+        current_x = Protein.aminoCoordinates[focus][x]
+        current_y = Protein.aminoCoordinates[focus][y]
+        plotX.append(current_x)
+        plotY.append(current_y)
+
+        if proteinChain[focus] == 'H':
+            plot.scatter(current_x, current_y,color='r',s=500,zorder=1)
+        else:
+            plot.scatter(current_x, current_y,color='b',s=500,zorder=1)
 
         # Iterates over all the other amino acids in the chain
         for partner in range(len(proteinChain)):
 
-            neighbor_x = output['x-cor'][partner]
-            neighbor_y = output['y-cor'][partner]
+            neighbor_x = Protein.aminoCoordinates[partner][x]
+            neighbor_y = Protein.aminoCoordinates[partner][y]
 
             # Checks whether the 'partner' amino acid neighbors the 'focus' amino acids
-            # and checks whether these can form an H-bond
+            # , checks whether these can form an H-bond, and plots those
             if (((abs(current_x - neighbor_x) == 1) and\
             (abs(current_y - neighbor_y) == 0)) or\
             ((abs(current_y - neighbor_y) == 1) and\
             (abs(current_x - neighbor_x) == 0))) and\
-            (output['type'][focus] == 'H' and output['type'][partner] == 'H'):
+            (proteinChain[focus] == 'H' and proteinChain[partner] == 'H'):
                 plot.plot([current_x, neighbor_x],[current_y,neighbor_y]\
                 , 'r--', linewidth=0.5, zorder=-1)
 
     # Plots the covalent bonds in the protein
-    plot.plot(output['x-cor'],output['y-cor'], linewidth = 5.0, zorder = 0)
-
-    # Plots the single amino acids as H's (red) or P's (blue)
-    for acids in range(len(proteinChain)):
-        if output['type'][acids] == 'H':
-            plot.scatter(output['x-cor'][acids], output['y-cor'][acids]\
-            , color='r', s=500, zorder=1)
-        else:
-            plot.scatter(output['x-cor'][acids], output['y-cor'][acids]\
-            , color='b', s=500, zorder=1)
+    plot.plot(plotX, plotY, linewidth = 5.0, zorder = 0)
 
     # Graph info
-    plot.title('Protein shizzle')
+    plot.title('Strength: ' + str(Protein.strength))
     plot.ylabel('Y axis')
     plot.xlabel('X axis')
 
     plot.show()
 
-def calculateFolding (self, values, amount):
+# Calculates the protein stability/strength
+def calculateFolding (aminoCoordinates, proteinChain):
+
+    x = 0
+    y = 1
     strength = 0
-    for amino in range(amount):
-        location = self.exist[amino]
-        surrounding = [(location[0] - 1, location[1]), (location[0] + 1, location[1]), (location[0], location[1] + 1), (location[0], location[1] - 1)]
-        if values['type'][amino] == 'H':
-            for checks in range(amount):
-                if values['type'][checks] == "H" and self.exist[checks] in surrounding and (amino - checks) not in [-1, 0, 1]:
-                    strength += 0.5
-    self.bestFolding(strength, self.proteinDict, self.tries)
+
+    # Iterates over all single amino acids in the protein
+    for focus in range(len(proteinChain)):
+
+        current_x = aminoCoordinates[focus][x]
+        current_y = aminoCoordinates[focus][y]
+
+        # Iterates over all the other amino acids in the chain
+        for partner in range(len(proteinChain)):
+
+            neighbor_x = aminoCoordinates[partner][x]
+            neighbor_y = aminoCoordinates[partner][y]
+
+            # Checks whether the 'partner' amino acid neighbors the 'focus' amino acids
+            # , checks whether these can form an H-bond, and plots those
+            if (((abs(current_x - neighbor_x) == 1) and\
+            (abs(current_y - neighbor_y) == 0)) or\
+            ((abs(current_y - neighbor_y) == 1) and\
+            (abs(current_x - neighbor_x) == 0))) and\
+            (proteinChain[focus] == 'H' and proteinChain[partner] == 'H'):
+                strength += 0.5
+
+    return strength
