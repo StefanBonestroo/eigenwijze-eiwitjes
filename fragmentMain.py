899f397e9cb52d12bff829e02d764c2e82bc6b8c
[Yente Stor] <[yentestor@gmail.com]>
2017-12-05 13:35:22 +0100

    fragmentmain

--- fragmentMain.py
+++ fragmentMain.py
@@ -8,14 +8,14 @@ from Algorithms.fragmentRandomizer import fragmentRandomizer
 
 def main():
 
-    eggwhite = Protein('HPHPPHHPHPPHPPHHHHHHHHPPPH')
+    eggwhite = Protein('PPPHHPPHHPPPPPHHHHHHHPPHHPPPPHHPPHPP')
 
     # Runs the algorithm function (10 tries)
-    output = randomizer(eggwhite, 100)
-    print(output)
+    output = randomizer(eggwhite, 100, '2D')
     eggwhite.aminoCoordinates = output[0]
     eggwhite.strength = output[1]
-    fragmentRandomizer(eggwhite, 4)
+    fragmentRandomizer(eggwhite, 4, '2D', 10)
+
 
 
     # visualizeFolding(eggwhite)
