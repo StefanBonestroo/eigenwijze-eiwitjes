53e8bf4c4d3b75b014b9512834f758e52cdbcfd1
[Yente Stor] <[yentestor@gmail.com]>
2017-12-12 18:33:54 +0100

    (͡° ͜ʖ ͡°) werkende fragment randomizer!

--- Algorithms/fragmentRandomizer.py
+++ Algorithms/fragmentRandomizer.py
@@ -1,6 +1,7 @@
 # This function takes a folded protein and refolds a fragment of a defined length
 # in a randomized matter.
 import random
+import math
 
 from classes import Protein
 from random import randint
@@ -20,20 +21,20 @@ def fragmentRandomizer (inputPro, fragment, dimension, trieMax):
 
     # Get the best protein from a 100 random foldings
 
-    output = randomizer(inputPro, 1000, dimension)
+    output = randomizer(inputPro, 10000, dimension)
     inputPro.aminoCoordinates = output[0]
     inputPro.strength = output[1]
 
     origPro = inputPro
     # print(origPro.aminoCoordinates)
-    oldScore = origPro.strength # waar is dit voor dan?
+    bestPro = origPro # waar is dit voor dan?
     # print(oldScore)
     # error if something went wrong in randomizer
     if len(origPro.aminoCoordinates) != len(origPro.proteinChain):
         raise Exception('proteinlength does not correspond to length of aminoCoordinates')
 
-    tries = 0
-    while tries < trieMax:
+    temp = 3.5
+    while temp > 0.1:
 
         # define start as random amino acid at least a fragment length before the end of the protein.
         start = len(origPro.proteinChain)
@@ -48,8 +49,6 @@ def fragmentRandomizer (inputPro, fragment, dimension, trieMax):
         elif(start < len(origPro.proteinChain) - fragment):
             middleInfo = middleFragment(origPro, start, fragment, dimension)
             newCoordinates = middleInfo[0]
-        # else() iets fout gegaan
-        #     error
 
         if newCoordinates != 'none':
                 # create protein with these coordinates for the fragments
@@ -76,14 +75,16 @@ def fragmentRandomizer (inputPro, fragment, dimension, trieMax):
                 newPro.strength = calculateFolding(newPro.aminoCoordinates, newPro.proteinChain)
                 # calculate probability of acceptance
 
-                if newPro.strength >= origPro.strength:
-                    if newPro.strength > origPro.strength:
-                        # print(newPro.strength)
-                        visualizeFolding(newPro)
-
+                probab =  min(1,(math.expm1(newPro.strength/temp)/math.expm1(origPro.strength/temp)))
+                randumb = random.uniform(0,1)
+                if probab > randumb:
+                    print(newPro.strength)
                     origPro = newPro
+                    if origPro.strength > bestPro.strength:
+                        bestPro = origPro
+                temp *= 0.9998
 
-        tries +=1
+
     visualizeFolding(origPro)
     return(origPro.aminoCoordinates, origPro.strength)
 
