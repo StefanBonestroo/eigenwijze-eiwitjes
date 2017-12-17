8515ab358d323fe4baadd905c21a0898e07f5bbc
[Yente Stor] <[yentestor@gmail.com]>
2017-12-17 21:10:45 +0100

    added visualizeFoldingSave wich does not shows but saves the folding in solutionquality

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+import os
+
 import matplotlib
 import matplotlib.pyplot as plot
 import matplotlib.axes as axis
@@ -136,3 +138,129 @@ class Protein:
             plot3D.axis('off')
 
             plot.show()
+
+    def visualizeFoldingSave(self, algorithm, proteinLabel, experiment):
+        x = 0
+        y = 1
+        z = 2
+
+        plotX = []
+        plotY = []
+        plotZ = []
+
+        if len(self.aminoCoordinates[0]) == 2:
+
+            # Initiate figure
+            plot2D = plot.figure()
+
+            # Iterates over all single amino acids in the protein
+            for focus in range(len(self.proteinChain)):
+
+                currentX = self.aminoCoordinates[focus][x]
+                currentY = self.aminoCoordinates[focus][y]
+                plotX.append(currentX)
+                plotY.append(currentY)
+
+                if self.proteinChain[focus] == 'H':
+                    plot.scatter(currentX,currentY,color='r',s=100,zorder=1)
+                elif self.proteinChain[focus] == 'C':
+                    plot.scatter(currentX,currentY,color='y',s=100,zorder=1)
+                else:
+                    plot.scatter(currentX,currentY,color='b',s=100,zorder=1)
+
+                # Iterates over all the other amino acids in the chain
+                for partner in range(len(self.proteinChain)):
+
+                    neighborX = self.aminoCoordinates[partner][x]
+                    neighborY = self.aminoCoordinates[partner][y]
+
+                    # Checks whether the 'partner' amino acid neighbors the 'focus' amino acids
+                    # , checks whether these can form an H-bond, and plots those
+                    if (((abs(currentX - neighborX) == 1) and\
+                    (abs(currentY - neighborY) == 0)) or\
+                    ((abs(currentX - neighborX) == 0) and\
+                    (abs(currentY - neighborY) == 1))) and\
+                    (focus - partner) not in [-1, 0, 1]:
+                        if (self.proteinChain[focus] == 'H' and self.proteinChain[partner] == 'H') or\
+                        (self.proteinChain[focus] == 'C' and self.proteinChain[partner] == 'H') or\
+                        (self.proteinChain[focus] == 'H' and self.proteinChain[partner] == 'C'):
+                            plot.plot([currentX, neighborX],[currentY,neighborY]\
+                            , 'r--', linewidth=1, zorder=-1)
+                        elif self.proteinChain[focus] == 'C' and self.proteinChain[partner] == 'C':
+                            plot.plot([currentX, neighborX],[currentY,neighborY]\
+                            , 'y--', linewidth=1, zorder=-1)
+
+            # Plots the covalent bonds in the protein
+            plot.plot(plotX, plotY, linewidth = 0.5, zorder = 0)
+
+            # Graph info
+            plot.title('Strength: ' + str(self.strength))
+            plot.axis('off')
+
+            plot.show()
+
+
+        elif len(self.aminoCoordinates[0]) == 3:
+
+            # Initiate 3D plot
+            figure3D = plot.figure()
+            plot3D = figure3D.add_subplot(111, projection = '3d')
+
+            # Iterates over all single amino acids in the protein
+            for focus in range(len(self.proteinChain)):
+
+                currentX = self.aminoCoordinates[focus][x]
+                currentY = self.aminoCoordinates[focus][y]
+                currentZ = self.aminoCoordinates[focus][z]
+                plotX.append(currentX)
+                plotY.append(currentY)
+                plotZ.append(currentZ)
+
+                if self.proteinChain[focus] == 'H':
+                    plot3D.scatter(currentX,currentY,currentZ,c='r',s=100,zorder=1)
+                elif self.proteinChain[focus] == 'C':
+                    plot3D.scatter(currentX,currentY,currentZ,c='y',s=100,zorder=1)
+                else:
+                    plot3D.scatter(currentX,currentY,currentZ,c='b',s=100,zorder=1)
+
+                # Iterates over all the other amino acids in the chain
+                for partner in range(len(self.proteinChain)):
+
+                    neighborX = self.aminoCoordinates[partner][x]
+                    neighborY = self.aminoCoordinates[partner][y]
+                    neighborZ = self.aminoCoordinates[partner][z]
+
+                    # Checks whether the 'partner' amino acid neighbors the 'focus' amino acids
+                    # , checks whether these can form an H-bond, and plots those
+                    if (((abs(currentX - neighborX) == 1) and\
+                    (abs(currentY - neighborY) == 0) and\
+                    (abs(currentZ - neighborZ) == 0)) or\
+                    ((abs(currentX - neighborX) == 0) and\
+                    (abs(currentY - neighborY) == 1) and\
+                    (abs(currentZ - neighborZ) == 0)) or\
+                    ((abs(currentX - neighborX) == 0) and\
+                    (abs(currentY - neighborY) == 0) and\
+                    (abs(currentZ - neighborZ) == 1))) and\
+                    (focus - partner) not in [-1, 0, 1]:
+                        if (self.proteinChain[focus] == 'H' and self.proteinChain[partner] == 'H') or\
+                        (self.proteinChain[focus] == 'C' and self.proteinChain[partner] == 'H') or\
+                        (self.proteinChain[focus] == 'H' and self.proteinChain[partner] == 'C'):
+                            plot3D.plot([currentX,neighborX],[currentY,neighborY],\
+                            [currentZ,neighborZ], 'r--', linewidth=1, zorder=-1)
+                        elif self.proteinChain[focus] == 'C' and self.proteinChain[partner] == 'C':
+                            plot3D.plot([currentX,neighborX],[currentY,neighborY],\
+                            [currentZ,neighborZ], 'y--', linewidth=1, zorder=-1)
+
+            # Plots the covalent bonds in the protein
+            plot3D.plot(plotX, plotY, plotZ, linewidth = 0.5, zorder = 0)
+
+            # Graph info
+            plot3D.text2D(0.5, 0.95,'Strength: ' + str(self.strength), transform = plot3D.transAxes)
+            plot3D.axis('off')
+
+            #
+            myPath = os.path.dirname(os.path.abspath(__file__))
+            myFile = '/Data/solution-quality/' + 'Protein-' + algorithm + '-' + proteinLabel + experiment + '.png'
+
+            plot.savefig(myPath + myFile)
+            plot.clf()
